2d599dde6d11fbfb1dc6d78ad1fdbacb5b6b3da7
LucasdBenitez1 <lucasdbenitez@gmail.com>
2024-01-02 18:36:23 -0300

    library finish

---
+++ script.js
@@ -0,0 +1,64 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  displayBooks();
+}
+
+function displayBooks() {
+  const libraryContainer = document.getElementById('library-container');
+  libraryContainer.innerHTML = '';
+
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement('div');
+    card.classList.add('book-card');
+
+    card.innerHTML = `
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.read ? 'Yes' : 'No'}</p>
+      <button onclick="removeBook(${index})">Remove</button>
+      <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
+    `;
+
+    libraryContainer.appendChild(card);
+  });
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+  displayBooks();
+}
+
+document.getElementById('new-book-btn').addEventListener('click', () => {
+  const formContainer = document.getElementById('form-container');
+  formContainer.style.display = 'block';
+
+  document.getElementById('book-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+
+    document.getElementById('book-form').reset();
+    formContainer.style.display = 'none';
+  });
+});
